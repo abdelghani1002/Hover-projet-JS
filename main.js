d3d20cfd93f1fbe9e340c663d000012320f5c494
abdelghani <aaittamghart8@gmail.com>
2023-02-11 10:19:27 +0100

    Change icon to Morroco flag

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
 let colors = ['red','green','aqua','yellow','gray','#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 
-for(let i=1;i<8053;i++){
+for(let i=1; i<8053; i++){
     let div = document.createElement('div');
     div.classList.add('cercle');
 
     div.onmouseenter = ()=>{
         let color = colors[Math.floor( Math.random()*colors.length )];
         div.style.backgroundColor = color;
-        div.style.boxShadow = `0 0 0 4px ${color}`;
+        div.style.boxShadow = `0 0 0 5px ${color}`;
     }
     
     div.onmouseleave = ()=>{
